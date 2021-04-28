0b51935952de116086929628ef34e6f336b66c95
Sebas-PM <jspena85@misena.edu.co>
2021-04-28 11:31:46 -0500

    o

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,30 @@ Route::get('arreglos', function (){
     var_dump($estudiantes);
     echo"</pre>";
 });
+route::get("paises",function(){
+    $paises = ["Colombia" =>[
+                                "capital" => "Bogotá",
+                                "moneda" => "Peso",
+                                "poblacion" => 51
+                            ],
+                "Peru" =>   [
+                                "capital" => "lima",
+                                "moneda" => "sol",
+                                "poblacion" => 32
+                            ],
+                "Paraguay"=>[
+                                "capital" => "asuncion",
+                                "moneda" => "guarani",
+                                "poblacion" => 7
+                            ]];
+    foreach($paises as $nombre => $pais){
+        echo "<pre>";
+        echo "<h1>$nombre</h1>";
+        print_r($pais["capital" ]. "<br />");
+        print_r($pais["moneda"]."<br />");
+        print_r($pais["poblacion"]."<br />");
+        echo "</pre>";
+        echo "<hr />";
+    }
+});
 //definir variable en php
